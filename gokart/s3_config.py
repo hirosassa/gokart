7307a6030ce114b050b1d03072f06e14476ede2e
hirosassa <hiro.sassa@gmail.com>
2026-05-04 17:51:49 +0900

    fix: review comment

--- gokart/s3_config.py
+++ gokart/s3_config.py
@@ -1,8 +1,12 @@
 from __future__ import annotations
 
 import os
+from typing import TYPE_CHECKING
 
 import luigi
+
+if TYPE_CHECKING:
+    import luigi.contrib.s3
 
 
 class S3Config(luigi.Config):
@@ -11,16 +15,18 @@ class S3Config(luigi.Config):
 
     _client = None
 
-    def get_s3_client(self):
+    def get_s3_client(self) -> luigi.contrib.s3.S3Client:
         if self._client is None:  # use cache as like singleton object
             self._client = self._get_s3_client()
         return self._client
 
-    def _get_s3_client(self):
+    def _get_s3_client(self) -> luigi.contrib.s3.S3Client:
         try:
-            import luigi.contrib.s3
+            import boto3  # noqa: F401
         except ImportError:
             raise ImportError('S3 support requires additional dependencies. Install them with: pip install gokart[s3]') from None
+        import luigi.contrib.s3
+
         return luigi.contrib.s3.S3Client(
             aws_access_key_id=os.environ.get(self.aws_access_key_id_name), aws_secret_access_key=os.environ.get(self.aws_secret_access_key_name)
         )
